fa0c9c65dc1c6debf56431149469ddc4f7594c19
Faruk Erat <92527106+FarukErat@users.noreply.github.com>
2022-04-03 21:18:33 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "replace.h"
+
+int main()
+{
+    char text[] = "Hello World";
+    char rep[] = "World";
+    char with[] = "Universe";
+    replace(text, rep, with);
+    // "!" is to see if there is any character in the end of the string
+    // to make sure the code runs properly
+    printf("%s!", text); // prints "Hello Universe!"
+    return 0;
+}
